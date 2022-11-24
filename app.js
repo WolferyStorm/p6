eaf862ca651b3a048d35b7d07a0363f9ae2a0219
Clet Nathan <nanouclet@gmail.com>
2022-11-24 14:08:51 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://Storm:159@cluster0.e6fqxmp.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res) => {
+   res.json({ message: 'Votre requête a bien été reçue !' }); 
+});
+
+module.exports = app;
