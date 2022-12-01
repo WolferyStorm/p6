d8715d7bba9093e2341d1cdadc2de4599eb28d6f
Clet Nathan <nanouclet@gmail.com>
2022-12-01 14:14:58 +0100

    user done start sauce

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const sauce = require('./models/sauce');
+const userRoutes = require('./routes/user');
 
-const app = express();
-
-mongoose.connect('mongodb+srv://Storm:159@cluster0.e6fqxmp.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://Storm:159@cluster0.e6fqxmp.mongodb.net/test?',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+const app = express();
 
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
+app.use('/api/sauces', (req, res, next) => {
+  sauce.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+});
+app.use(express.json());
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
