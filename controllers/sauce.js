7b2640709ecabc786ae52cdf1c6ee9c6a2212296
Clet Nathan <nanouclet@gmail.com>
2022-12-08 15:55:45 +0100

    error

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -26,14 +26,27 @@ exports.createSauce =(req, res, next) => {
       .catch(error => res.status(400).json({ error }));
   };
 exports.modifySauce =(req, res, next) => {
-    const sauceObject = req.file ?
-    {
+    const sauceObject = req.file ? {
         ...JSON.parse(req.body.sauce),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`} : { ...req.body }
-    sauce.updateOne({ _id : req.params.id}, {...sauceObject, _id: req.params.id})
-      .then(() => res.status(200).json({ message: 'Sauce modifié !'}))
-      .catch(error => res.status(400).json({ error }));
-  };
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+    sauce.findOne({_id: req.params.id})
+        .then((sauce) => {
+            console.log(Sauce)
+            if (sauce.userId._id.toString() !== sauceObject.userId) {
+                res.status(401).json({ message : 'Vous ne pouvez pas modifié cette sauce'});
+            } else {
+                console.log(Sauce)
+                sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
+                console.log(Sauce)
+                .then(() => res.status(200).json({message : 'Sauce modifié!'}))
+                .catch(error => res.status(401).json({ error }));
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error });
+        });
+ };
 exports.deleteSauce = (req, res, next) => {
     sauce.findOne({ _id: req.params.id})
         .then(sauce => {
